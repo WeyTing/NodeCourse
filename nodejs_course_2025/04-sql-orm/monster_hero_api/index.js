d73817a4073b14231dc3aee9f819ef755f9d6995
wey <qaz237562@gmail.com>
2025-05-16 16:08:05 +0800

    操作資料庫 in vs

--- nodejs_course_2025/04-sql-orm/monster_hero_api/index.js
+++ nodejs_course_2025/04-sql-orm/monster_hero_api/index.js
@@ -1,21 +1,23 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const heroesRouter = require('./src/routes/heroes')
-const monstersRouter = require('./src/routes/monsters')
+const express = require("express");
+const app = express();
+const cors = require("cors"); // 跨域 cors
+const heroesRouter = require("./src/routes/heroes");
+const monstersRouter = require("./src/routes/monsters");
 
 // app.use(cors())
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  method: ['GET', 'POST', 'PUT', 'DELETE']
-}))
+app.use(
+	cors({
+		origin: "http://localhost:5173", // 設定允許的來源
+		method: ["GET", "POST", "PUT", "DELETE"], // 設定允許的請求方法
+	})
+);
 
-app.use(express.json())
-app.use('/heroes', heroesRouter)
-app.use('/monsters', monstersRouter)
+app.use(express.json());
+app.use("/heroes", heroesRouter);
+app.use("/monsters", monstersRouter);
 
-const PORT = 3000
+const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`)
-})
+	console.log(`server running on port ${PORT}`);
+});
--- nodejs_course_2025/04-sql-orm/monster_hero_api/src/routes/heroes.js
+++ nodejs_course_2025/04-sql-orm/monster_hero_api/src/routes/heroes.js
@@ -1,61 +1,68 @@
-const express = require("express")
-const router = express.Router()
-const db = require("../../db/index")
-const { heroesTable, monstersTable } = require("../../db/schema")
-const { eq } = require("drizzle-orm")
+const express = require("express");
+const router = express.Router();
+const db = require("../../db/index");
+const { heroesTable, monstersTable } = require("../../db/schema");
+const { eq } = require("drizzle-orm");
 // get all
 router.get("/", async (req, res) => {
-  try {
-    const rows = await db.select().from(heroesTable)
-    res.json(rows)
-  } catch (err) {
-    res.status(500).json({ error: err.message })
-  }
-})
+	try {
+		const rows = await db.select().from(heroesTable);
+		res.json(rows);
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+});
 
 // get by id
 router.get("/:id", async (req, res) => {
-  try {
-    const rows = await db.select().from(heroesTable).where(eq(heroesTable.id, req.params.id))
+	try {
+		const rows = await db
+			.select()
+			.from(heroesTable)
+			.where(eq(heroesTable.id, req.params.id)); // 指定哪個table欄位 動態id
+		// select * from heroes where id = req.params.id 動態id
+		if (!rows) return res.status(404).json({ message: "找不到符合 ID" });
 
-    if (!rows) return res.status(404).json({ message: "找不到符合 ID" })
-
-    res.json(rows)
-  } catch (err) {
-    res.status(500).json({ error: err.message })
-  }
-})
+		res.json(rows);
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+});
 
 // update
 router.put("/:id", async (req, res) => {
-  try {
-    const { name, gender, age, hero_level, hero_rank, description } = req.body
+	try {
+		const { name, gender, age, hero_level, hero_rank, description } = req.body;
 
-    await db
-      .update(heroesTable)
-      .set({
-        name,
-        gender,
-        age,
-        hero_level,
-        hero_rank,
-        description
-      })
-      .where(eq(heroesTable.id, req.params.id))
-
-    res.json({ message: "更新英雄成功" })
-  } catch (err) {
-    res.status(500).json({ error: err.message })
-  }
-})
+		await db
+			.update(heroesTable)
+			.set({
+				name,
+				gender,
+				age,
+				hero_level,
+				hero_rank,
+				description,
+			})
+			.where(eq(heroesTable.id, req.params.id));
+		// UPDATE heroes
+		// set age = 10
+		// where id = 動態id
+		res.json({ message: "更新英雄成功" });
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+});
 
 router.delete("/:id", async (req, res) => {
-  try {
-    await db.delete(heroesTable).where(eq(heroesTable.id, req.params.id))
-    res.json({ message: "成功刪除" })
-  } catch (err) {
-    res.status(500).json({ error: err.message })
-  }
-})
+	try {
+		await db.delete(heroesTable).where(eq(heroesTable.id, req.params.id));
+		// DELETE FROM heroes
+		// WHERE id = 動態id
+		res.json({ message: "成功刪除" });
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+});
 
-module.exports = router
+module.exports = router;
