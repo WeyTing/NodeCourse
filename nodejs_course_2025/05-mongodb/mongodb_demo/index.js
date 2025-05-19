4ff958faac26144f62f94756febfd06d1f1ecf55
wey <qaz237562@gmail.com>
2025-05-19 12:19:09 +0800

    寫入CRUD

--- nodejs_course_2025/05-mongodb/mongodb_demo/index.js
+++ nodejs_course_2025/05-mongodb/mongodb_demo/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const app = express();
 const port = 3000;
 require("dotenv").config();
+const userRoutes = require("./routes/user"); // 引入 user 路由
 
 const connectDB = require("./config/db"); // 模組化 MongoDB 連接
 connectDB(); // 連接到 MongoDB
 
+const app = express();
+app.use(express.json()); // 解析 JSON 請求主體
+app.use("/api", userRoutes); // 使用 user 路由
+
 app.listen(port, () => {
-	console.log(`運作在 ${port}`);
+	console.log(`伺服器已啟動在 http://localhost:${port}`);
 });
